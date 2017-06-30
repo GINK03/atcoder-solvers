cd1023d183718818eba10b7055e25d083e60785b
gimpei <gimpei@1-1-06-583.local>
2017-06-30 20:39:35 +0900

    updae

---
+++ abc016_c.kt
@@ -0,0 +1,37 @@
+
+fun main( args : Array<String> ) {
+  val (m, n) = readLine()!!.split(" ").map { it.toInt() }
+  val mm     = mutableMapOf<Int, MutableList<Int>>()
+  (1..n).map {
+    val (a, b) = readLine()!!.split(" ").map { it.toInt() }
+    if( mm[a] == null ) mm[a] = mutableListOf<Int>()
+    if( mm[b] == null ) mm[b] = mutableListOf<Int>()
+
+    mm[a]!!.add(b)
+    mm[b]!!.add(a)
+  }
+  // search friends of friends...
+  val nn = mutableMapOf<Int, MutableSet<Int>>()
+  mm.map { kvs ->
+    val (k, vs) = kvs
+    nn[k] = mutableSetOf<Int>() 
+    vs.map { v -> 
+       mm[v]!!
+        .filter { x -> 
+          x != k &&
+          !mm[k]!!.contains(x)
+        }
+        .map { x ->
+          nn[k]!!.add( x )
+        }
+    }
+  }
+  nn.toList()
+  .sortedBy { kv ->
+    val (k, v) = kv
+    k
+  }.map { kv ->
+    val (k, v) = kv
+    println(v.size)
+  }
+}
