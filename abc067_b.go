e36b0a6d7d9e7a54d4b11a9f844916f5c8101800
gimpei <gim.kobayashi@gmail.com>
2017-07-21 02:41:59 +0900

    bbb

--- abc067_b.go
+++ abc067_b.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "sort"
-  "strconv"
-  "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
 
 func read() string {
-  sc.Scan()
-  var input = sc.Text()
-  return input
+	sc.Scan()
+	var input = sc.Text()
+	return input
 }
 
 func main() {
-  input := strings.Split(read(), " ")
-  k, _ := strconv.Atoi(input[1])
-  arr := strings.Split(read(), " ")
-  iarr := make([]int, 0)
-  for _, a := range arr {
-    i, _ := strconv.Atoi(a)
-    iarr = append(iarr, i)
-  }
-  sort.Ints(iarr)
-  sum := 0
-  for _, i := range iarr[len(iarr)-1*k:] {
-    sum += i
-  }
-  fmt.Println(sum)
+	input := strings.Split(read(), " ")
+	k, _ := strconv.Atoi(input[1])
+	arr := strings.Split(read(), " ")
+	iarr := make([]int, 0)
+	for _, a := range arr {
+		i, _ := strconv.Atoi(a)
+		iarr = append(iarr, i)
+	}
+	sort.Ints(iarr)
+	sum := 0
+	for _, i := range iarr[len(iarr)-1*k:] {
+		sum += i
+	}
+	fmt.Println(sum)
 }
