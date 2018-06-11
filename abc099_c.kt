d869510b1150e854d63c84d184601c5b620196be
nardtree <gim.kobayashi@gmail.com>
2018-06-11 15:06:18 +0900

    update

--- abc099_c.kt
+++ abc099_c.kt
@@ -2,27 +2,14 @@
 fun main(args:Array<String>) {
   val n = readLine()!!.toDouble() 
   
-
-  var no = n 
-  var c = 0
-  while(true) {
-    if( no < 6.0 ) break
-    
-    val a1 = ( Math.log(no) / Math.log(9.0) ).toInt()
-      
-    val a2 = ( Math.log(no)/ Math.log(6.0) ).toInt()
-    
-    val nextA1 = no - Math.pow(9.0, a1.toDouble())
-    val nextA2 = no - Math.pow(6.0, a2.toDouble())
-    
-    if( nextA1 < nextA2 ) { 
-      no = nextA1
-    } else {
-      no = nextA2 
-    }
-    c += 1
-    //println("a1 $a1 a2 $a2 no $no")
+  var res = n;
+  for( i in (0..n.toInt()-1) ) {
+    var cc = 0
+    var t = i
+    while(t>0) { cc+=t%6; t/=6 }
+    t=(n-i).toInt()
+    while(t>0) { cc+=t%9; t/=9 }
+    if(res>cc) res=cc.toDouble()
   }
-  //println(no)
-  println((no+c).toInt())
+  println( res.toInt() )
 }
